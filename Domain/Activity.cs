fc8624e6b97d32379fd4d1b7201a59bf5be40d32
dnaita <deepaknaita@gmail.com>
2021-09-16 20:05:16 +0530

    End of Section 5

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -19,4 +19,5 @@ namespace Domain
         public string City { get; set; }
      
     }
+   
 }
---
+++ client-app/src/app/models/activity.tsx
@@ -0,0 +1,9 @@
+export interface Activity {
+    id: string;
+    title: string;
+    date: string;
+    category: string;
+    description: string;
+    venue: string;
+    city: string;
+}
