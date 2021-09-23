0fecf8cb609338954ce7665af9ec716965cd595d
dnaita <deepaknaita@gmail.com>
2021-09-23 22:02:22 +0530

    End of Section 7

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -10,7 +10,7 @@ const sleep =(delay: number)=>{
 axios.defaults.baseURL="http://localhost:5000/api";
 axios.interceptors.response.use(async response=>{
       try {
-        await sleep(1000);
+        await sleep(100);
         return response;
     } catch (error) {
         console.log(error);
